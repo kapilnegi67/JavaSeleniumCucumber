049c7b8eaebaece5fbfbdc9e039fb36db7d1e7de
Dell <Dell@DESKTOP-JR3SS3H>
2023-06-10 10:54:14 +0530

    rectified cucumber

---
+++ src/main/java/Runner/simpleloginFeatureRunner.java
@@ -0,0 +1,17 @@
+package Runner;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		monochrome = true,
+		plugin = {"html:target/cucumber-html-report"},
+		features = "src/main/java/Features/simplelogin.feature", // It tells where your feature files are, full path is also acceptable here
+		glue = {"stepDefinitions/LoginFeature"} // It tells where your step Definition files are, full path is also acceptable here
+		)
+public class simpleloginFeatureRunner {
+
+}
--- target/cucumber-html-report/report.js
+++ target/cucumber-html-report/report.js
@@ -1,169 +1,99 @@
-$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:src/main/java/Features/taggedHooks.feature");
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:src/main/java/Features/parameterizeUsingMap.feature");
 formatter.feature({
-  "name": "Tagged Hooks Demo",
+  "name": "Free CRM Login Feature With Parameterize Map",
   "description": "",
   "keyword": "Feature"
 });
 formatter.scenario({
-  "name": "This is first test",
+  "name": "With Parameterize Map Free CRM Login Test Scenario",
   "description": "",
-  "keyword": "Scenario",
-  "tags": [
+  "keyword": "Scenario"
+});
+formatter.step({
+  "name": "User is already on Login Page",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "ParameterizeStepDefinition.user_is_already_on_Login_Page()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "title of Login Page is Cogmento CRM",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "ParameterizeStepDefinition.title_of_Login_Page_is_Cogmento_CRM()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User enters username and password",
+  "rows": [
     {
-      "name": "@First"
-    }
-  ]
-});
-formatter.before({
-  "status": "passed"
-});
-formatter.before({
-  "status": "passed"
-});
-formatter.before({
-  "status": "passed"
-});
-formatter.step({
-  "name": "StepOne",
-  "keyword": "Given "
-});
-formatter.match({
-  "location": "TaggedHooksStepDefinition.stepone()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "StepTwo",
-  "keyword": "Given "
-});
-formatter.match({
-  "location": "TaggedHooksStepDefinition.steptwo()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "StepThree",
-  "keyword": "Given "
-});
-formatter.match({
-  "location": "TaggedHooksStepDefinition.stepthree()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.after({
-  "status": "passed"
-});
-formatter.after({
-  "status": "passed"
-});
-formatter.after({
-  "status": "passed"
-});
-formatter.scenario({
-  "name": "This is second test",
-  "description": "",
-  "keyword": "Scenario",
-  "tags": [
+      "cells": [
+        "username",
+        "password"
+      ]
+    },
     {
-      "name": "@Second"
-    }
-  ]
-});
-formatter.before({
-  "status": "passed"
-});
-formatter.before({
-  "status": "passed"
-});
-formatter.step({
-  "name": "StepOne",
-  "keyword": "Given "
-});
-formatter.match({
-  "location": "TaggedHooksStepDefinition.stepone()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "StepTwo",
-  "keyword": "Given "
-});
-formatter.match({
-  "location": "TaggedHooksStepDefinition.steptwo()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "StepThree",
-  "keyword": "Given "
-});
-formatter.match({
-  "location": "TaggedHooksStepDefinition.stepthree()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.after({
-  "status": "passed"
-});
-formatter.after({
-  "status": "passed"
-});
-formatter.scenario({
-  "name": "This is third test",
-  "description": "",
-  "keyword": "Scenario",
-  "tags": [
+      "cells": [
+        "kapil_negi67@yahoo.com",
+        "Freecrm@2020"
+      ]
+    },
     {
-      "name": "@Third"
+      "cells": [
+        "kapil_negi671@yahoo.com",
+        "Freecrm1@2020"
+      ]
+    },
+    {
+      "cells": [
+        "abc@def.com",
+        "somepassword"
+      ]
     }
-  ]
-});
-formatter.before({
-  "status": "passed"
-});
-formatter.before({
-  "status": "passed"
-});
-formatter.step({
-  "name": "StepOne",
-  "keyword": "Given "
+  ],
+  "keyword": "Then "
 });
 formatter.match({
-  "location": "TaggedHooksStepDefinition.stepone()"
+  "location": "ParameterizeStepDefinition.user_enters_username_and_password(DataTable)"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "StepTwo",
-  "keyword": "Given "
+  "name": "User clicks on Login button",
+  "keyword": "Then "
 });
 formatter.match({
-  "location": "TaggedHooksStepDefinition.steptwo()"
+  "location": "ParameterizeStepDefinition.user_clicks_on_Login_button()"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "StepThree",
-  "keyword": "Given "
+  "name": "User is on Home Page",
+  "keyword": "Then "
 });
 formatter.match({
-  "location": "TaggedHooksStepDefinition.stepthree()"
+  "location": "ParameterizeStepDefinition.user_is_on_Home_Page()"
 });
 formatter.result({
-  "status": "passed"
+  "error_message": "org.openqa.selenium.NoSuchElementException: no such element: Unable to locate element: {\"method\":\"xpath\",\"selector\":\"//span[@class\u003d\u0027user-display\u0027][text()\u003d\u0027kapil negi\u0027]\"}\n  (Session info: chrome\u003d113.0.5672.129)\nFor documentation on this error, please visit: https://www.seleniumhq.org/exceptions/no_such_element.html\nBuild info: version: \u00273.141.59\u0027, revision: \u0027e82be7d358\u0027, time: \u00272018-11-14T08:17:03\u0027\nSystem info: host: \u0027DESKTOP-JR3SS3H\u0027, ip: \u0027192.168.1.66\u0027, os.name: \u0027Windows 10\u0027, os.arch: \u0027amd64\u0027, os.version: \u002710.0\u0027, java.version: \u002717.0.6\u0027\nDriver info: org.openqa.selenium.chrome.ChromeDriver\nCapabilities {acceptInsecureCerts: false, browserName: chrome, browserVersion: 113.0.5672.129, chrome: {chromedriverVersion: 113.0.5672.63 (0e1a4471d5ae..., userDataDir: C:\\Users\\Dell\\AppData\\Local...}, goog:chromeOptions: {debuggerAddress: localhost:57888}, javascriptEnabled: true, networkConnectionEnabled: false, pageLoadStrategy: normal, platform: WINDOWS, platformName: WINDOWS, proxy: Proxy(), setWindowRect: true, strictFileInteractability: false, timeouts: {implicit: 0, pageLoad: 300000, script: 30000}, unhandledPromptBehavior: dismiss and notify, webauthn:extension:credBlob: true, webauthn:extension:largeBlob: true, webauthn:extension:minPinLength: true, webauthn:extension:prf: true, webauthn:virtualAuthenticators: true}\nSession ID: 06902e3fcdcfd219c6d7dae89b64b044\n*** Element info: {Using\u003dxpath, value\u003d//span[@class\u003d\u0027user-display\u0027][text()\u003d\u0027kapil negi\u0027]}\r\n\tat java.base/jdk.internal.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)\r\n\tat java.base/jdk.internal.reflect.NativeConstructorAccessorImpl.newInstance(NativeConstructorAccessorImpl.java:77)\r\n\tat java.base/jdk.internal.reflect.DelegatingConstructorAccessorImpl.newInstance(DelegatingConstructorAccessorImpl.java:45)\r\n\tat java.base/java.lang.reflect.Constructor.newInstanceWithCaller(Constructor.java:499)\r\n\tat java.base/java.lang.reflect.Constructor.newInstance(Constructor.java:480)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.createException(W3CHttpResponseCodec.java:187)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:122)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:49)\r\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:158)\r\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.execute(DriverCommandExecutor.java:83)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:552)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:323)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElementByXPath(RemoteWebDriver.java:428)\r\n\tat org.openqa.selenium.By$ByXPath.findElement(By.java:353)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:315)\r\n\tat stepDefinitions.ParameterizeFeature.ParameterizeStepDefinition.user_is_on_Home_Page(ParameterizeStepDefinition.java:51)\r\n\tat ✽.User is on Home Page(file:src/main/java/Features/parameterizeUsingMap.feature:12)\r\n",
+  "status": "failed"
 });
-formatter.after({
-  "status": "passed"
+formatter.step({
+  "name": "close the browser",
+  "keyword": "Then "
 });
-formatter.after({
-  "status": "passed"
+formatter.match({
+  "location": "ParameterizeStepDefinition.close_the_browser()"
+});
+formatter.result({
+  "status": "skipped"
 });
 });
